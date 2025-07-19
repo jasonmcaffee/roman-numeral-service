06aa391ed3532c5465e16f5f8536286638990090
jasonmcaffee <jasonlmcaffee@gmail.com>
2025-07-19 15:08:42 -0600

    fix error when running in dev instead of docker.

--- src/app.module.ts
+++ src/app.module.ts
@@ -37,7 +37,7 @@ const services = loadModules(join(__dirname, 'services'), '.service.js');
     LoggerModule.forRoot({
       pinoHttp: {
         level: appConfig.isProduction() ? 'trace' : 'info',
-        stream: pino.destination('/app/logs/roman-numeral-service.log'),
+        stream: pino.destination('/tmp/roman-numeral-service.log'),
       },
     }),
     DatadogTraceModule.forRoot(),
